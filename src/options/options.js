1bf1e0ff5c39832feed2577b5322581111fae1ed
Niklaus Hofer <niklaus.hofer@stepping-stone.ch>
2021-04-28 22:38:06 +0200

    Add three new options to the settings page.
    This commit does not contain code that uses the new options. The code
    will follow in a later commit.
    
    There are three new settings introduced:
    
     - alwaysShowFullPath: Show the full path to the folder even if there is
       no other folder of the same name.
     - alwaysShowMailbox: Show the mailbox the folder belongs to, even if
       there is no folder of the same name in any other mailbox.
     - doNotCrop: Do not crop the folder names, even if they are very long.
    
    These options are supposed to improve the usability when working with
    many mailboxes and huge directory structures.

--- src/options/options.js
+++ src/options/options.js
@@ -8,6 +8,9 @@
     markAsRead: true,
     maxRecentFolders: 15,
     excludeArchives: false,
+    alwaysShowFullPath: false,
+    alwaysShowMailbox: false,
+    doNotCrop: false,
   });
 
   for (let [name, value] of Object.entries(prefs)) {
@@ -39,6 +42,9 @@
       maxRecentFolders: parseInt(document.getElementById("maxRecentFolders").value, 10),
       markAsRead: document.getElementById("markAsRead").checked,
       excludeArchives: document.getElementById("excludeArchives").checked,
+      alwaysShowFullPath: document.getElementById("alwaysShowFullPath").checked,
+      alwaysShowMailbox: document.getElementById("alwaysShowMailbox").checked,
+      doNotCrop: document.getElementById("doNotCrop").checked,
     });
   });
 })();
