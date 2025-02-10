9d79c74451ec8f490b976431abab39860726b1f9
mic <m@micz.it>
2025-02-10 23:11:56 +0100

    feat: Added a notification for each operation

--- src/common/util.js
+++ src/common/util.js
@@ -9,6 +9,8 @@ export const DEFAULT_PREFERENCES = {
   migratedShiftArrow: false,
   recentStrategy: "accessed",
   partialMatchFullPath: false,
+  notificationFirstRunDone: false,
+  notificationActive: false,
 };
 
 export async function getValidatedFolders(rootNode, prefName) {
@@ -26,4 +28,36 @@ export async function getValidatedFolders(rootNode, prefName) {
 export function cmdOrCtrlKey(event) {
   const isMac = navigator.platform.toUpperCase().includes("MAC");
   return isMac ? event.metaKey : event.ctrlKey;
+}
+
+export async function showNotification(title, message, dismissTime = 10000) {
+  let notificationID = await browser.notifications.create(null, {
+    type: "basic",
+    title: title,
+    iconUrl: browser.runtime.getURL("images/addon-atn.png"),
+    message: message
+  });
+  if (dismissTime > 0) {
+    setTimeout(() => {
+      browser.notifications.clear(notificationID);
+    }, dismissTime);
+  }
+}
+
+export function createNotificationText(operation, numMessages, destination, firstRunDone = true) {
+  let notificationMessage = browser.i18n.getMessage("operation_action_" + operation) + " " + numMessages + " " + browser.i18n.getMessage("operation_message_" + (numMessages>1?"plural":"single"))+".";
+  if (firstRunDone == false) {
+    notificationMessage = browser.i18n.getMessage("notification_first_run") + " " + notificationMessage;
+    browser.storage.local.set({ notificationFirstRunDone: true });
+  }
+  switch (operation) {
+    case "copy":
+    case "move":
+      notificationMessage += " Folder: " + destination;
+      break;
+    case "tag":
+      notificationMessage += " Tag: " + destination;
+      break;
+  }
+  return notificationMessage;
 }
